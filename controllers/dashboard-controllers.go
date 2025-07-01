8c2c642e82671e68c25da6cbd9755d1d23dab193
Mohammad <mohammad.ah.pr@gmail.com>
2025-07-01 10:42:09 +0330

    Redesign routes (part2), Edit controllers

--- controllers/dashboard-controllers.go
+++ controllers/dashboard-controllers.go
@@ -1,50 +1,26 @@
 package controllers
 
 import (
-	"Tasktop/models"
-	"fmt"
-	"log"
 	"net/http"
-	"strconv"
 )
 
 func DashHandler(w http.ResponseWriter, r *http.Request) {
 	TemplateRender(w, "/dashboard/dashboard", nil)
 }
 
-func DailyPlan(w http.ResponseWriter, r *http.Request) {
+//Read Goals
+
+func DailyGoal(w http.ResponseWriter, r *http.Request) {
+
+}
+func MonthlyGoal(w http.ResponseWriter, r *http.Request) {
+
+}
+func AnnuallyGoal(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func MonthlyPlan(w http.ResponseWriter, r *http.Request) {
-
-}
-
-func AnnuallyPlan(w http.ResponseWriter, r *http.Request) {
-	TemplateRender(w, "/dashboard/annually-plans", nil)
-}
-func AnnuallyGoals(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "GET" {
-		TemplateRender(w, "/dashboard/annually-goals", nil)
-	} else if r.Method == "POSt" {
-		if err := r.ParseForm(); err != nil {
-			log.Println(err)
-			return
-		}
-		title := r.FormValue("title")
-		desc := r.FormValue("description")
-		annaullyPId := r.FormValue("APID")
-		conv, err := strconv.Atoi(annaullyPId)
-		if err != nil {
-			log.Println(err)
-			return
-		}
-		annuallyG := &models.AnnuallyGoal{Title: title, Desc: desc, Status: true, APID: conv}
-		models.AddAnnuallyG(annuallyG)
-	}
-}
-
-//Create Controllers
+//Create Goals
 
 func CDailyGoal(w http.ResponseWriter, r *http.Request) {
 	TemplateRender(w, "/dashboard/create-goals/daily/create-update", nil)
@@ -54,27 +30,57 @@ func CMonthlyGoal(w http.ResponseWriter, r *http.Request) {
 	TemplateRender(w, "/dashboard/create-goals/monthly/create-update", nil)
 }
 func CAnnuallyGoal(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "GET" {
-		TemplateRender(w, "/dashboard/create-goals/annually/create-update", nil)
-	} else if r.Method == "POST" {
-		if err := r.ParseForm(); err != nil {
-			log.Println(err)
-			return
-		}
-		title := r.FormValue("title")
-		desc := r.FormValue("description")
-		annuallyPId, err := models.GetAnnuallyPByUserId(1)
-		if err != nil {
-			log.Println(err)
-		}
-		annuallyG := &models.AnnuallyGoal{Title: title, Desc: desc, Status: true, APID: annuallyPId.APID}
-		models.AddAnnuallyG(annuallyG)
-		r.Method = "GET"
-		AnnuallyGoals(w, r)
-	} else {
-		fmt.Println("dd")
-	}
+	// if r.Method == "GET" {
+	// 	TemplateRender(w, "/dashboard/create-goals/annually/create-update", nil)
+	// } else if r.Method == "POST" {
+	// 	if err := r.ParseForm(); err != nil {
+	// 		log.Println(err)
+	// 		return
+	// 	}
+	// 	title := r.FormValue("title")
+	// 	desc := r.FormValue("description")
+	// 	annuallyPId, err := models.GetAnnuallyPByUserId(1)
+	// 	if err != nil {
+	// 		log.Println(err)
+	// 	}
+	// 	annuallyG := &models.AnnuallyGoal{Title: title, Desc: desc, Status: true, APID: annuallyPId.APID}
+	// 	models.AddAnnuallyG(annuallyG)
+	// 	r.Method = "GET"
+	// 	AnnuallyGoals(w, r)
+	// } else {
+	// 	fmt.Println("dd")
+	// }
 }
+
+//Update Goals
+
+func UDailyGoal(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func UMonthlyGoal(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func UAnnuallyGoals(w http.ResponseWriter, r *http.Request) {
+
+}
+
+//Delete Goals
+
+func DDailyGoal(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func DMonthlyGoal(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func DAnnuallyGoal(w http.ResponseWriter, r *http.Request) {
+
+}
+
+//Report
 
 func Report(w http.ResponseWriter, r *http.Request) {
 
--- routes/dashboard-routes.go
+++ routes/dashboard-routes.go
@@ -10,9 +10,9 @@ var DashRegister = func(r *mux.Router) {
 	r.HandleFunc("/dashboard", controllers.DashHandler)
 
 	//Read Goals
-	r.HandleFunc("/dashboard/daily-goals/{goalId:[0-99]+}", controllers.DailyPlan).Methods("GET")
-	r.HandleFunc("/dashboard/monthly-goals/{goalId:[0-99]+}", controllers.MonthlyPlan).Methods("GET")
-	r.HandleFunc("/dashboard/annually-goals/{goalId:[0-99]+}", controllers.AnnuallyGoals).Methods("GET")
+	r.HandleFunc("/dashboard/daily-goals/{goalId:[0-99]+}", controllers.DailyGoal).Methods("GET")
+	r.HandleFunc("/dashboard/monthly-goals/{goalId:[0-99]+}", controllers.MonthlyGoal).Methods("GET")
+	r.HandleFunc("/dashboard/annually-goals/{goalId:[0-99]+}", controllers.AnnuallyGoal).Methods("GET")
 
 	//Create Goals
 	r.HandleFunc("/dashboard/daily-goals/create", controllers.CDailyGoal).Methods("POST")
@@ -20,14 +20,14 @@ var DashRegister = func(r *mux.Router) {
 	r.HandleFunc("/dashboard/annually-goals/create", controllers.CAnnuallyGoal).Methods("POST")
 
 	//Update Goals
-	r.HandleFunc("/dashboard/daily-goals/{goalId:[0-99]+}", controllers.DailyPlan).Methods("PUT")
-	r.HandleFunc("/dashboard/monthly-goals/{goalId:[0-99]+}", controllers.MonthlyPlan).Methods("PUT")
-	r.HandleFunc("/dashboard/annually-goals/{goalId:[0-99]+}", controllers.AnnuallyGoals).Methods("PUT")
+	r.HandleFunc("/dashboard/daily-goals/{goalId:[0-99]+}", controllers.UDailyGoal).Methods("PUT")
+	r.HandleFunc("/dashboard/monthly-goals/{goalId:[0-99]+}", controllers.UMonthlyGoal).Methods("PUT")
+	r.HandleFunc("/dashboard/annually-goals/{goalId:[0-99]+}", controllers.UAnnuallyGoals).Methods("PUT")
 
 	//Delete Goals
-	r.HandleFunc("/dashboard/daily-goals/{goalId:[0-99]+}", controllers.DailyPlan).Methods("DELETE")
-	r.HandleFunc("/dashboard/monthly-goals/{goalId:[0-99]+}", controllers.MonthlyPlan).Methods("DELETE")
-	r.HandleFunc("/dashboard/annually-goals/{goalId:[0-99]+}", controllers.AnnuallyGoals).Methods("DELETE")
+	r.HandleFunc("/dashboard/daily-goals/{goalId:[0-99]+}", controllers.DDailyGoal).Methods("DELETE")
+	r.HandleFunc("/dashboard/monthly-goals/{goalId:[0-99]+}", controllers.DMonthlyGoal).Methods("DELETE")
+	r.HandleFunc("/dashboard/annually-goals/{goalId:[0-99]+}", controllers.DAnnuallyGoal).Methods("DELETE")
 
 	//Report
 	r.HandleFunc("/dashboard/report", controllers.Report)
